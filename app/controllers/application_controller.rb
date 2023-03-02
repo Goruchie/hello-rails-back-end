6fbca204064f265aad72634da8405fe238e7b452
Goruchie <marcosgualtero43253@gmail.com>
2023-03-01 21:54:58 -0300

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET'
+  end
 end
